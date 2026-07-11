60320fc7fbd801ff39db34d95e7dbaeb603fbe4c
PratyushBhattarai <bhattaraipratyush2061@gmail.com>
2026-07-11 09:29:58 +0545

    Add PostgreSQL integration for video detector

--- detector/admin.py
+++ detector/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import VideoUpload, DetectedShape
 
-# Register your models here.
+@admin.register(VideoUpload)
+class VideoUploadAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "original_name",
+        "status",
+        "created_at",
+        "processed_at",
+    )
+    list_filter = ("status",)
+    search_fields = ("original_name",)
+
+
+@admin.register(DetectedShape)
+class DetectedShapeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "shape_name",
+        "video",
+        "frame_number",
+        "timestamp_seconds",
+    )
+    list_filter = ("shape_name",)
